a39d9d045f5e2556701f936975f8f427ac07d8e6
Anish Pitchuka <AnishPitchuka8@gmail.com>
2025-09-05 16:30:46 +0530

    fix threadcard and recommend comps

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import Recommend from '../../components/Recommend/Recommend'
+import CommunityCard from '../../components/ThreadCard/ThreadCard'
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <div className="trending-title">Trending Threads</div>
+      <CommunityCard></CommunityCard>
       <Recommend> </Recommend>
+      
     </div>
   )
 }
