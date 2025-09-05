81d6ebf2af777eeed6ec03687a4ebe02a85fc8e5
Harish Pullagurla <harish140707@gmail.com>
2025-09-05 14:43:29 +0530

    added files

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div>
+      <image src='https://previews.123rf.com/images/fokaspokas/fokaspokas1803/fokaspokas180300231/96761297-message-cloud-icon-white-icon-with-shadow-on-transparent-background.jpg'></image>
+    </div>
+  )
+}
+
+export default Navbar
