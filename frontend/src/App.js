6edf35a5d42c95d3e7e6eba975676784af3e2d5f
Anish Pitchuka <anishpitchuka8@gmail.com>
2025-09-05 16:05:37 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
+
 import './App.css';
 import Home from './pages/Home/Home'
 function App() {
   return (
     <div className="App">
      <Home></Home>
-=======
-
-import './App.css';
-import CommunityCard from './components/ThreadCard/ThreadCard';
-
-function App() {
-  return (
-    <div className="App">
-      <CommunityCard></CommunityCard>
->>>>>>> anish
     </div>
   );
 }
-
 export default App;
+
+
